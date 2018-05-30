2285abfc78ff090cd9dd0347d606091150e5e268
Paul W <weerheim.paul@gmail.com>
2018-05-31 00:20:50 +0200

    Updated subrepo's, added manual com port selection, and order of connecting items to make sure the 3 devices (gyro, lidar, and haptic) are not mixed up in terms of com ports.

--- app.py
+++ app.py
@@ -1,9 +1,97 @@
 from detectionfields import *
+from Haptic.HapticHandler import *
+
+import sys
+import glob
+
 import time
 
-if __name__ == '__main__':
-    x = DetectionFields()
-    x.start_updating()
-    while True:
-        print(x.latest_output)
+gyro_port = lidar_port = haptic_port = None
+
+
+def find_serial_ports():
+    global gyro_port, lidar_port, haptic_port
+    old = serial_ports()
+    input('Connect Gyroscope and press ENTER')
+
+    new = serial_ports()
+    diff = list(set(new) - set(old))
+    if len(diff) < 1:
+        print('No (new) USB/COM ports found! Exiting!')
+        exit()
+    else:
+        gyro_port = diff[0]
+        print('Found', diff[0])
+        old = new
+
+    input('Connect Lidar and press ENTER')
+    new = serial_ports()
+    diff = list(set(new) - set(old))
+    if len(diff) < 1:
+        print('No (new) USB/COM ports found! Exiting!')
+        exit()
+    else:
+        lidar_port = diff[0]
+        print('Found', diff[0])
+        old = new
+
+    input('Connect Haptic and press ENTER')
+    new = serial_ports()
+    diff = list(set(new) - set(old))
+    if len(diff) < 1:
+        print('No (new) USB/COM ports found! Exiting!')
+        exit()
+    else:
+        haptic_port = diff[0]
+        print('Found', diff[0])
+
+        old = new
+
+
+def main():
+    find_serial_ports()
+
+    df = DetectionFields(lidar_port, gyro_port)
+    hp = HapticHandler(haptic_port)
+
+    df.start_updating()
+    while df.thread_is_active:
+        output = df.latest_output
+        print(output)
+        for o in output:
+            if o is not None:
+                hp.activate(o[2], o[0])
         time.sleep(1)
+
+
+def serial_ports():
+    """ Lists serial port names
+        :raises EnvironmentError:
+            On unsupported or unknown platforms
+        :returns:
+            A list of the serial ports available on the system
+    """
+    if sys.platform.startswith('win'):
+        ports = ['COM%s' % (i + 1) for i in range(256)]
+    elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
+        # this excludes your current terminal "/dev/tty"
+        ports = glob.glob('/dev/tty[A-Za-z]*')
+    elif sys.platform.startswith('darwin'):
+        ports = glob.glob('/dev/tty.*')
+    else:
+        raise EnvironmentError('Unsupported platform')
+
+    result = []
+    for port in ports:
+        try:
+            s = serial.Serial(port)
+            s.close()
+            result.append(port)
+        except (OSError, serial.SerialException):
+            pass
+    return result
+
+
+if __name__ == '__main__':
+    main()
+    print('End of program')
--- detectionfields.py
+++ detectionfields.py
@@ -1,3 +1,5 @@
+import math
+
 from gyroLidar.gydar import *
 
 
@@ -5,15 +7,21 @@ class DetectionFields(object):
     amount_of_fields = 7
     degrees_front = 0
     fov = 180
+    warning_degrees = 30
 
     _gydar = None
     _gydar_thread = None
 
+    lidar_data_is_trusted = True
+    latest_gyro = [None, None]
+    latest_bottom = None
     latest_output = [None for _ in range(amount_of_fields)]
     thread_is_active = False
 
-    def __init__(self):
+    def __init__(self, lidar_port, gyro_port):
         self._gydar = Gydar()
+        self._gydar.set_lidar_port(lidar_port)
+        self._gydar.set_gyro_port(gyro_port)
 
     def start_updating(self):
         self.thread_is_active = True
@@ -45,10 +53,48 @@ def gydar_loop(gydar: Gydar, fields: DetectionFields):
         end = end % data_length
 
     while fields.thread_is_active:
-        # for the time being no ping sensor data in there.
-        gyro_data = gydar.raw_gyro_output
+        """ If some sensors or USB connections got fucked """
+        if gydar.connected != ConnectionStates.BOTH_CONNECTED:
+            if gydar.connected == ConnectionStates.GYRO_CONNECTED:
+                gydar.disconnect_gyro()
+            elif gydar.connected == ConnectionStates.LIDAR_CONNECTED:
+                gydar.disconnect_lidar()
 
+            fields.thread_is_active = False
+
+        """ Get data """
+
+        ping_data, gyro_data = gydar.raw_gyro_output
         lidar_data = gydar.raw_lidar_output
+
+        """ Check if degrees are greater than e.g. 30 degrees, then we shouldn't trust the lidar. """
+
+        is_trusted = True
+        if gyro_data is not None:
+            for deg in gyro_data:
+                if deg > fields.warning_degrees:
+                    is_trusted = False
+
+        fields.lidar_data_is_trusted = is_trusted
+
+        """ Analyse pingsensor data"""
+
+        front = left = right = 1e10
+        try:
+            front = ping_data[0] * 10  # 0 degrees front
+            left = ping_data[1] * 10  # 45 degrees left
+            right = ping_data[2] * 10  # 45 degrees right
+            fields.latest_down = ping_data[3] * 10  # pointing down
+        except TypeError:
+            pass
+
+        degree_per_field = fields.fov / fields.amount_of_fields
+
+        left_field = math.floor((fields.fov / 2 - 45) / degree_per_field)
+        right_field = fields.amount_of_fields - left_field
+        middle_field = fields.amount_of_fields
+
+        """ Update fields """
 
         if start > end:
             lidar_data = lidar_data[start:] + lidar_data[:end]
@@ -60,10 +106,21 @@ def gydar_loop(gydar: Gydar, fields: DetectionFields):
         index = 0
         for field in lidar_data:
             try:
-                field.sort(reverse=True)
+                field.sort()
                 average = sum(field) / len(field)
                 closest = field[0]
-                fields.latest_output[index] = (average, closest)
+
+                if index == middle_field:
+                    if middle < closest:
+                        closest = front
+                elif index == left_field:
+                    if left < closest:
+                        closest = left
+                elif index == right_field:
+                    if right < closest:
+                        closest = right
+
+                fields.latest_output[index] = (average, closest, index * degree_per_field)
             except TypeError:
                 pass
             index += 1
